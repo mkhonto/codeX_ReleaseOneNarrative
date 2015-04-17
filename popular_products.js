cf93ac71dd195ed5a2b6d1a58ddfd5bc65235a56
mkhonto <jabulani@codex.co>
2015-04-17 09:09:07 +0200

    done with Nelisa's first three stories

--- popular_products.js
+++ popular_products.js
@@ -1,62 +1,161 @@
-    var fs = require('fs');
+ var fs = require('fs');
 
-    module.exports = function(filePath){
-        this.productNames = function(callback) {
-            var linesInFile = fs.readFileSync(filePath, "utf8"); 
-            var lines= linesInFile.split('\r');
-            var totalProducts =[];
+  module.exports = function(){
 
-            lines.forEach(function(storedLines){
-                var product = storedLines.split(';');
-                
-                var currentItem = product[2];
-                var productTotal = product[3];
+    this.productList = function(filePath){
 
-                var productMap = {
-                    itemName : currentItem,
-                    soldItems : Number(productTotal)
-                };
-                totalProducts.push(productMap);
-              });
+      var linesInFile = fs.readFileSync(filePath, "utf8");
 
-            callback(null, totalProducts);
+      var rows = linesInFile.split('\r');
+      //console.log(rows.length);
+
+      var listOfProducts = [];
+      
+      rows.forEach(function(row){
+        var columns = row.split(';');
+        var currentItem = columns[2];
+        var numberSold = columns[3];
+
+        var salesObj = {
+          itemName : columns[2],
+          soldItems: Number(columns[3])
         };
-        this.groupedItems = function(){
-            var linesInFile =     fs.readFileSync(filePath, "utf8");
-            var productLines = linesInFile.split('\r');
-            var productCountMap = {};
-            productLines.forEach(function(productLine){
+        listOfProducts.push(salesObj);
+      });
+      return listOfProducts;
+    }
 
-                var splitLines = productLine.split(';');
-                console.log(splitLines);
+    this.groupItems = function(products) {
+      var itemMap = {};
 
-                var currentItem = splitLines[2];
-                var numberSold =  splitLines[3];
+      products.forEach(function(products) {
+        var currentItem = products.itemName;
+        var numberSold = products.soldItem;
 
-                if(productCountMap[currentItem] === undefined)
-                {
-                        productCountMap[currentItem] = 0;
-                }
-                    productCountMap[currentItem] += Number(numberSold);
-            });
-            
-            return productCountMap;
-        };
-        
-        this.mostPopular = function(productCountMap){
-            var mostPopularProduct = {};
-            var max = 0;
-            for(var prop in productCountMap) {
-                var value = productCountMap[prop];
-                if(value > max) {
-                      max = value;
-                    mostPopularProduct = {
-                       Product : prop,
-                       Amount  : max
-                    }
-                }
-            }
-             //console.log(itemMap);
-              return mostPopularProduct ;
-        };
+        if(itemMap[currentItem] === undefined){
+          itemMap[currentItem] = 0;
+        }
+
+        itemMap[currentItem] = itemMap[currentItem] + Number(numberSold);
+
+      });
+      return itemMap;
+
     };
+
+        //Think about creating a list of objects from the csv
+           //Create a function that find the most popular product, put it in a module and write a unit test.
+           this.mostPopularPdt = function(itemMap){
+             var mostPopularProduct = {};
+             var max = 0;
+             for(var prop in itemMap) {
+               var value = itemMap[prop];
+               if(itemMap[prop] > max) {
+                max = itemMap[prop];
+                mostPopularProduct = {
+                 name : prop,
+                 amt  : max
+               }
+             }
+           }
+           //console.log(itemMap);
+           return mostPopularProduct;
+         };
+
+         this.leastPopularPdt = function(itemMap){
+           var leastPopularProduct = {};
+           var min = 172;
+           for(var prop in itemMap) {
+             var value = itemMap[prop];
+             if(itemMap[prop] < min) {
+              min = itemMap[prop];
+              leastPopularProduct = {
+               name : prop,
+               amt  : min
+             }
+           }
+         }
+           //console.log(itemMap);
+           return leastPopularProduct;
+         };
+
+   this.groupCat = function(categories) {
+
+      var productCategories = {
+      'Milk 1l': 'Dairy Products',
+      'Imasi': 'Dairy Products',
+      'Bread': 'Bakery',
+      'Chakalaka Can': 'Canned Food',
+      'Gold Dish Vegetable Curry Can': 'Canned Food',
+      'Fanta 500ml': 'Cold Beverages',
+      'Coke 500ml': 'Cold Beverages',
+      'Cream Soda 500ml': 'Cold Beverages',
+      'Iwisa Pap 5kg': 'Bulk',
+      'Top Class Soy Mince': 'Soup',
+      'Shampoo 1 litre': 'Cosmetics',
+      'Soap Bar': 'Cosmetics',
+      'Bananas - loose': 'Fruit',
+      'Apples - loose': 'Fruit',
+      'Mixed Sweets 5s': 'Confectionery',
+      'Heart Chocolates': 'Confectionery',
+      'Rose (plastic)': 'Valentines Goodies',
+      'Valentine Cards': 'Valentines Goodies'
+    }
+        var catMap = {};
+
+       categories.forEach(function(categories) {
+      //get the category for the current product!!!!!!
+        var category = categories.itemName;
+        var prodQty = categories.soldItems;
+
+       if (catMap[category] === undefined){
+        catMap[category] = 0;
+      }
+      // get the quantity for the current product
+      
+      //add the qty to the correct category 
+      catMap[category] = catMap[category] + Number(prodQty); 
+    });
+
+    //console.log(catMap);
+    return catMap
+  };
+
+  this.mostPopularCat = function(catMap){
+    var mostPopularCategory = {};
+    var max = 0;
+
+    for(var prop in catMap) {
+      var value = catMap[prop];
+      if(catMap[prop] > max){
+        max = catMap[prop];
+        mostPopularCategory = {
+          name: prop,
+          amt: max
+        }
+      }
+    }
+    //console.log(mostPopularCategory)
+    return mostPopularCategory;
+  }; 
+
+  this.leastPopularCat = function(catMap){
+           var leastPopularCategory = {};
+           var min = 328;
+           for(var prop in catMap) {
+             var value = catMap[prop];
+             if(catMap[prop] < min) {
+              min = catMap[prop];
+              leastPopularCategory = {
+               name : prop,
+               amt  : min
+             }
+           }
+         }
+         console.log(leastPopularCategory);
+         return leastPopularCategory;
+    };
+};
+
+
+   
